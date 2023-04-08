182be7db6a1756cf52b71b53943d6f983019ce7c
Joshua <e0544333@u.nus.edu>
2023-04-09 00:14:29 +0800

    Initial commit

---
+++ index.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import os
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+import nltk
+import sys
+import getopt
+import pickle
+import shutil
+import sys
+import collections
+import math
+import time
+
+def usage():
+    print("usage: " +
+          sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+
+
+def build_index(in_dir, out_dict, out_postings):
+    """
+    build index from documents stored in the input directory,
+    then output the dictionary file and postings file
+    """
+    print('indexing...')
+
+    # get the start time
+    st = time.time()
+    
+    # This is an empty method
+    # Pls implement your code in below
+
+input_directory = output_file_dictionary = output_file_postings = None
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o == '-i':  # input directory
+        input_directory = a
+    elif o == '-d':  # dictionary file
+        output_file_dictionary = a
+    elif o == '-p':  # postings file
+        output_file_postings = a
+    else:
+        assert False, "unhandled option"
+
+if input_directory == None or output_file_postings == None or output_file_dictionary == None:
+    usage()
+    sys.exit(2)
+
+# increaseRecursionLimit()
+build_index(input_directory, output_file_dictionary, output_file_postings)
---
+++ search.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import os
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+import nltk
+import sys
+import math
+import collections
+import pickle
+from nltk.stem.porter import PorterStemmer
+import getopt
+import collections
+import math
+import pickle
+import heapq
+
+stemmer = PorterStemmer()
+
+# python3 search.py -d dictionary.txt -p postings.txt -q queries.txt -o results.txt
+
+
+def usage():
+    print("usage: " +
+          sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
+
+def run_search(dict_file, postings_file, queries_file, results_file):
+    """
+    using the given dictionary file and postings file,
+    perform searching on the given queries file and output the results to a file
+    """
+    print('running search on the queries...')
+    # This is an empty method
+    # Pls implement your code in below
+
+dictionary_file = postings_file = file_of_queries = output_file_of_results = None
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:p:q:o:')
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o == '-d':
+        dictionary_file = a
+    elif o == '-p':
+        postings_file = a
+    elif o == '-q':
+        file_of_queries = a
+    elif o == '-o':
+        file_of_output = a
+    else:
+        assert False, "unhandled option"
+
+if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None:
+    usage()
+    sys.exit(2)
+
+run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
