8837b5e5fd35a6101cb40978cf622c91c352e86f
Joshua <e0544333@u.nus.edu>
2023-04-15 01:12:19 +0800

    add skeletal top k documents logic

--- query_parser.py
+++ query_parser.py
@@ -1,12 +1,25 @@
 from stack import Stack
+import math
+import heapq
+import collections
 
 class QueryParser:
 
     def __init__(self):
-        pass
+        # N represents the total number of articles in the dataset.
+        N = 0
+        K = 10
 
     def process_query(self, query):
+
         print("processing query...")
+
+        with open('document.txt', 'r') as f:
+            data = f.read().split()
+            # Set N as the total number of articles
+            N = int(data[0])
+            doc_lengths = [(int(data[i]), float(data[i+1])) for i in range(1, len(data), 2)]
+
 
         if 'AND' in query:
             results = self.process_boolean_query(query)
@@ -14,6 +27,9 @@ class QueryParser:
             results = self.process_freetext_query(query)
 
 
+    # ==========================================================================
+    # ====================== BOOLEAN QUERY PROCESSING ==========================
+    # ==========================================================================
     '''
     Process query terms by smallest doc_freq first for optimisation. Queries could be 
     - "fertility treatment" AND "sus words" AND chicken AND nuggets
@@ -61,12 +77,80 @@ class QueryParser:
     def is_phrase(self, string): 
         return string.count(' ') > 0
     
+
+    # ======================================================================
+    # ====================== FREE TEXT PROCESSING ==========================
+    # ======================================================================
+
+    def process_freetext_query(self, query):
+        # Collection to count the occurences of a term in a query
+        queryIndex = collections.defaultdict(lambda: 0)
+        # Collection to store the query term weight
+        query_weight_dict = collections.defaultdict(lambda: 0)
+
+
+        score_dict = collections.defaultdict(lambda: 0)
+
+        for word in query:
+            queryIndex[word] += 1
+        
+        square_val_list = []
+        for word in queryIndex:
+            postingList = self.get_postings_list(word)
+            query_term_weight = self.get_query_term_weight(word, queryIndex, len(postingList))
+            query_weight_dict[word] = query_term_weight
+            square_val_list.append(query_term_weight ** 2)
+        
+        square_val_list.sort()
+        square_sum = sum(square_val_list)
+
+        # Get the query normalization factor 
+        query_normalization_factor = math.sqrt(square_sum)
+
+        for term in queryIndex:
+            # get normalised query vector item
+            postingList = self.get_postings_list(term)
+            query_term_weight = query_weight_dict[term]
+            query_term_weight /= query_normalization_factor
+
+        # get normalised document vector item, then add score
+        # [documentId, frequency]
+        for frequencyPair in postingList:
+            currScore = document_term_weights_dict[frequencyPair[0]][term] * query_term_weight
+            score_dict[frequencyPair[0]] += currScore
+
+        self.get_top_k_components(score_dict)
+        return
+    
+    # ====================================================================
+    # ====================== RANKING PROCESSING ==========================
+    # ====================================================================
+
+    def get_query_term_weight(self, query_term, termIndex, postings_list_len):
+        if postings_list_len == 0:
+            return 0
+        return (1 + math.log(termIndex[query_term], 10)) * math.log(self.N/postings_list_len)
+
+    def get_document_term_weight(self, document_term_frequency):
+        return 1 + math.log(document_term_frequency, 10)
+        
+    def get_top_K_components(self, scores_dic):
+        result = []
+        score_tuples = [(-score, doc_id) for doc_id, score in scores_dic.items()]
+        
+        heapq.heapify(score_tuples)
+        for i in range(self.K):
+            tuple_result = heapq.heappop(score_tuples)
+            result.append(tuple_result[1])
+
+        return result
+
     '''
     Processes the phrasal query and returns a list of documents that are
     relevant.  
     '''
     def process_phrase(self, phrase):
-
+        return
 
     def get_postings_list(term):
-
+        return
