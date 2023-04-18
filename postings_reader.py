ae0eca861f4453a95fef37565828f5103cda532c
timothywongej <e0559903@u.nus.edu>
2023-04-18 20:47:39 +0800

    Add terminating clause when term not found

--- postings_reader.py
+++ postings_reader.py
@@ -16,7 +16,7 @@ class PostingsReader:
         if postings_ptr != -1: 
             with open("postings.txt", 'r') as f:
                 f.seek(postings_ptr, 0)
-                print(f.readline())
+                print("postings list retrieved:", f.readline())
 
         return postings_ptr
 
@@ -45,7 +45,6 @@ class PostingsReader:
             curr_block = self.pointer_data[mid].split(',') # E.g. 12,24968,41204,66916,209555
             
             dict_ptr = int(curr_block[0])
-            print(dict_ptr)
             f = open('dictionary.txt', 'r')
             f.seek(dict_ptr, 0) # Points to |7|carrara|7|carrati|8|carratti|5|carri...
 
@@ -66,7 +65,6 @@ class PostingsReader:
                         length_of_term += next_char
 
                 term = f.read(int(length_of_term))
-                print(length_of_term, term)
 
                 if i == 0:
                     first_term = term
@@ -84,43 +82,10 @@ class PostingsReader:
 
             elif query_term > last_term:
                 left = mid + 1
+
+            # Terminating clause when term is not found...
+            if right - left == 1:
+                break
         
         print("Term not found in dictionary...")
         return -1 
-
-    # def test(self):
-    #     with open('dictionary.txt', 'r', encoding='utf-8') as f:
-    #         print(len("z–".encode('utf-8')))
-    #         print(len("asdf".encode('utf-8')))
-    #         f.seek(3315, 0)
-    #         print(f.read(50))
-    #         f.seek(3324, 0)
-    #         print(f.read(20))
-    #         f.seek(3327, 0)
-    #         print(f.read(20))
-
-    #     with open('dictionary.txt', 'rb') as f:
-    #         f.seek(3320, 0) # Points to |7|carrara|7|carrati|8|carratti|5|carri...
-    #         print("Position:", 3320, "The next 20 chars are", f.read(20))
-    #         f.seek(2501, 0) # Points to |7|carrara|7|carrati|8|carratti|5|carri...
-    #         print("Position:", 2501, "The next 10 chars are", f.read(10))
-    #         f.seek(3325, 0) # Points to |7|carrara|7|carrati|8|carratti|5|carri...
-    #         data = f.read(10)
-    #         try:
-    #             decoded_data = data.decode('utf-8')
-    #         except UnicodeDecodeError as e:
-    #             print('Error:', e)
-    #             print('Invalid data:', data[e.start:e.end])
-    #         print("Position:", 3327, "The next 10 chars are", f.read(10))
-    #     return
-    
-    # def test2(self):
-    #     for i in range(200):
-    #         curr_block = self.pointer_data[i].split(',')
-    #         dict_ptr = int(curr_block[0])
-        
-    #         with open('dictionary.txt', 'r', encoding='utf-8') as f:
-    #             f.seek(int(dict_ptr), 0)
-    #             print(dict_ptr, f.read(30))
-
-    #     return
